71de2f2cef42d15ea97862d50945e7d43ba205de
ysnkd <kadirkol123@gmail.com>
2022-10-09 21:32:30 +0300

    Proje dosyası ekle.

---
+++ CleanCode/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace CleanCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Temiz Kod Yazma Sanatı
+            //Meslekte her zaman seni +1 yapar!
+            //SOLID prensiplerinde nasıl CleanCode yazabilirim?
+            //Kendimi nasıl geliştirebilirim?
+            //Robert C. Martin -> Clean Code
+
+            /*
+             * Soru: Projen ilerledikçe, değişen talepler, yeni özellikler projeye
+             * olan aidiyetinizi nasıl etkiliyor?
+             * Soru: Başkasının kodlarını okurken nasıl hissettiniz?
+             * Soru:Başkası sizin kodunuzu okursa diye endişe ettiniz mi?
+             * Soru:Geçen yıl yazdığınız koda bakınca ne hissediyorsunuz?
+ 
+             * KERVAN YOLDA DÜZÜLÜR mantığı tamamıyla kontrolsüz gelişigüzel kodlardır. İlk önce belirli
+             * diyagram oluşturup sistematik kod yazılmalıdır. (Solid geliştirme vs.) Overdesign'ın fazlası zarardır
+             * 5/3 oranlı olabilir.
+             */
+
+            /*1-Isimlendirme Kuralları----------
+             * Her dilin farklı isimlendirme kuralı vardır.
+             * 
+             * YANLIŞ : int s;
+             * 
+             * Aceleyle verilen değişken isim. Değişken isimleri açıklayıcı,pascalCase ile yazılmalıdır.
+             * 
+             * DOĞRU : int birinciSayi;
+             * 
+             * Common Language:IT süreçlerinde ORTAK dil.
+             * 
+             * PROGRAMLAMA DILLERINDE CLASSLAR DEĞİŞKENLER METOTLAR INGILIZCE OLMALIDIR.
+             * ORN: GETBY : VERİ GETİRME
+             *  
+             *  2-Fonksiyonlar
+             *  fonksiyon sadece bir iş yapsın.
+             *  Single Responsibility Principle,Code Refactoring
+             *  Refactoring Tool: ReSharper
+             *  
+             *  3-Yorumlar-Comments
+             *  Fonksiyonun içerisine comment yazılmaz.!
+             *  
+             *  4-Soyutlama
+             *  ÇIPLAK CLASS KALMASIN
+             * 
+             * Interface,Abstract ile beslenilmemiş class, değişime direnç gösterir.
+             * 
+             * 5-SOLID
+             * 
+             * Kodum SOLID, kafam rahat!
+             * 
+             * 6-Aspect Oriented Programming
+             * 
+             * public void Add(Product product)
+             * {
+             *    _productDal.Add(product)
+             * }
+             * 
+             * 
+
+
+
+
+
+        }
+    }
+}
